a518dd6acb2baf82aacba0f190a69eb707418965
zyborgLP <micu445micu@gmail.com>
2015-03-24 23:34:01 +0100

    forgot dis one

--- app/js/services/data-service.js
+++ app/js/services/data-service.js
@@ -9,7 +9,7 @@ servicesModule.service('dataService', function ($q, $timeout, sqliteService, $lo
     var service = {};
 
     service.getCurrentElements = function () {
-        var query = "SELECT * FROM CurrentElement AS ce " +
+        var query = "SELECT e.Id, e.Name, e.Description, e.Image, p.CurrentEnergy, ce.Location FROM CurrentElement AS ce " +
             "JOIN Player AS p ON p.Id = ce.PlayerId " +
             "JOIN Element AS e ON e.Id = ce.ElementId " +
             "WHERE p.Id = 1" +
@@ -18,6 +18,7 @@ servicesModule.service('dataService', function ($q, $timeout, sqliteService, $lo
         return sqliteService.query(query, []);
     };
 
+    // TODO: Use
     service.getAllElements = function () {
         var query = "SELECT * FROM Elements ";
         return sqliteService.query(query);
@@ -25,7 +26,7 @@ servicesModule.service('dataService', function ($q, $timeout, sqliteService, $lo
 
     service.getCombinedElement = function (element1, element2) {
         var parameters = [element1.Id, element2.Id, element1.Id, element2.Id];
-        var query = "SELECT * FROM Element AS e " +
+        var query = "SELECT e.Id, e.Name, e.Description, e.Image, r.EnergyUsage FROM Element AS e " +
             "JOIN Recipe AS r ON r.ResultId = e.Id " +
             "WHERE (r.Element1Id = ? AND r.Element2Id = ?) OR (r.Element2Id = ? AND r.Element1Id = ?)";
 
@@ -34,9 +35,9 @@ servicesModule.service('dataService', function ($q, $timeout, sqliteService, $lo
 
     service.getElementParts = function (element) {
         var parameters = [element.Id];
-        var query = "SELECT * FROM Element AS e " +
-            "JOIN Recipe AS r ON r.ResultId = e.Id " +
-            "WHERE r.ResultId = ? ";
+        var query = "SELECT e.Id, e.Name, e.Description, e.Image, r.EnergyUsage FROM Element AS e " +
+            "JOIN Recipe AS r ON r.Element1Id = e.Id OR r.Element2Id = e.Id " +
+            "WHERE r.ResultId = ?";
 
         return sqliteService.query(query, parameters);
     };
